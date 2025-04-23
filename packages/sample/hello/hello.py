cae7bd1e586428b556afe312ae33c2acef8d18bc
Dinesh Murthy <dineshm@microsoft.com>
2025-04-23 15:57:58 -0700

    Update hello.py
    DO

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,6 +1,6 @@
 def main(args):
       name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
+      greeting = "Hello " + name + "! welcome to DO"
       print(greeting)
       return {"body": greeting}
   
